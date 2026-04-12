81276d675bf179651131523ff20aae6499b659a8
Max Kapur <git@maxkapur.com>
2026-04-12 11:59:29 +0200

    Parse feed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use feedparser_rs::parse;
 use platform_dirs::AppDirs;
 use reqwest::{self, Error as ReqwestError};
 use std::fs::{read_to_string, write};
@@ -5,10 +6,18 @@ use std::fs::{read_to_string, write};
 const FEED_URL: &str = "https://manuelmoreale.com/feed/peopleandblogs";
 
 fn main() {
-    println!("Hello, world!");
-    dbg!(get_feed_cache_path());
     let feed_contents = get_feed_contents().unwrap();
-    dbg!(feed_contents);
+    let feed = parse(feed_contents.as_bytes()).unwrap();
+    feed.entries.iter().for_each(|entry| {
+        if let Some(d) = entry.summary_detail.as_ref() {
+            dbg!(d.clone().value);
+        }
+    });
+}
+
+fn get_feed_cache_path() -> std::path::PathBuf {
+    let app_dirs = AppDirs::new(Some("pnbx"), true).unwrap();
+    app_dirs.config_dir.join("config-file")
 }
 
 fn get_feed_contents() -> Result<String, ReqwestError> {
@@ -20,9 +29,4 @@ fn get_feed_contents() -> Result<String, ReqwestError> {
     let body = reqwest::blocking::get(FEED_URL).unwrap().text().unwrap();
     write(feed_cache_path, body.clone()).unwrap();
     Ok(body)
-}
-
-fn get_feed_cache_path() -> std::path::PathBuf {
-    let app_dirs = AppDirs::new(Some("pnbx"), true).unwrap();
-    app_dirs.config_dir.join("config-file")
 }
