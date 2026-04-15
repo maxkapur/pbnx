9af5314e8cc1bc6a644fe8468ca5ba3f4d6375a5
Max Kapur <git@maxkapur.com>
2026-04-15 18:18:54 +0200

    Compute Markov array

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::{
 use ndarray::*;
 
 const FEED_URL: &str = "https://manuelmoreale.com/feed/peopleandblogs";
+const DAMPING_FACTOR: f64 = 0.15;
 
 fn main() {
     let feed_contents = get_feed_contents().unwrap();
@@ -49,21 +50,39 @@ fn main() {
         .collect();
 
     let n = xrefs.len();
-    let mut array: Array2<f64> = Array2::zeros((n, n));
 
-    xrefs.iter().for_each(|(src_url, dest_urls)| {
-        let &j = url2idx.get(src_url).unwrap();
-
-        dest_urls.iter().for_each(|dest_url| {
-            let &i = url2idx.get(dest_url).unwrap();
-            array[[i, j]] = 1.0;
+    let xrefs_idx: Vec<HashSet<usize>> = idx2url
+        .iter()
+        .map(|src_url| {
+            xrefs[&src_url.clone()]
+                .iter()
+                .map(|dest_url| url2idx[dest_url])
+                .collect()
         })
+        .collect();
+
+    let d = DAMPING_FACTOR / (n as f64);
+    let nlinks: Vec<usize> = xrefs_idx.iter().map(|s| s.len()).collect();
+    let c: Vec<f64> = nlinks
+        .iter()
+        .map(|&m| (1.0 - DAMPING_FACTOR) / (m as f64))
+        .collect();
+    let one_over_n = 1.0 / (n as f64);
+
+    let markov_array: Array2<f64> = Array2::from_shape_fn((n, n), |(j, i)| {
+        if xrefs_idx[i].is_empty() {
+            // Then c[i] is +inf and column sum will be less than 1. Just assign
+            // equal probability to every transition
+            one_over_n
+        } else if xrefs_idx[i].contains(&j) {
+            d + c[i]
+        } else {
+            d
+        }
     });
 
-    let sum = array.sum_axis(Axis(0));
-
-    dbg!(array);
-    dbg!(sum);
+    let col_sum = markov_array.sum_axis(Axis(0));
+    assert!(col_sum.iter().all(|&x| (x - 1.0).abs() < 1e-8));
 }
 
 fn get_feed_cache_path() -> std::path::PathBuf {
